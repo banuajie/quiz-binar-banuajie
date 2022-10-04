54dc439b4c07d7dc2e2d616b40f96bf45e1b30a8
Nugraha Banuajie <nugrahabanuajie@gmail.com>
2022-10-04 22:16:50 +0700

    first commit

---
+++ quiz.js
@@ -0,0 +1,74 @@
+const text = "nama saya adalah budi, saya bekerja sebagai frontend engineer";
+
+const array = [1000, 20, 15, 18, 1, 9, 35];
+
+const isObject = {
+    nama_mobil: "ferrari",
+    kecepatan: "6000 cc",
+    harga: 20000000000,
+    tipe: "tidak diketahui",
+};
+const arrayOf = [
+    {
+        nama_mobil: "lamborgini",
+        kecepatan: "8000 cc",
+        harga: 70000000000,
+        tipe: "aventador",
+    },
+];
+
+//1 . gunakan metode replaceAll() pada input variable text diatas:
+//output: "nama saya adalah budi saya bekerja sebagai javascript engineer"
+
+//2. gunakan metode substring() pada input variable text diatas:
+// output : "saya bekerja sebagai frontend engineer"
+
+//3. gunakan metode toUpperCase() pada input variable text diatas:
+// output : "NAMA SAYA ADALAH BUDI, SAYA BEKERJA SEBAGAI FRONTEND ENGINEER"
+
+//4. gunakan metode sorting berdasarkan descending dan ascending pada input variable array diatas:
+// output : ascending :[1, 9, 15, 18, 20, 35, 1000]
+// output : descending: [1000, 35, 20, 18, 15, 9, 1]
+
+//5. gunakan metode indexOf() untuk pencarian nama pada input variable text diatas:
+// output : temukan string "frontend engineer" = (number of character)
+
+//7. gunakan metode includes() untuk pencarian nama pada input variable text diatas:
+// output : temukan string "frontend engineer" : (boolean value)
+
+//5. gunakan metode indexOf() untuk pencarian nama pada input variable array diatas:
+// output : temukan nilai 18 = [nilainya]
+
+//7. gunakan metode includes() untuk pencarian nama pada input variable array diatas:
+// output : temukan nilai 2000 = false
+
+//8. gunakan metode filter() untuk pencarian nama pada input variable array diatas:
+// output : hapus nilai selain 1000 = [35, 20, 18, 15, 9, 1]
+
+//9. gunakan metode filter() untuk pencarian nama pada input variable array diatas:
+// output : temukan nilai sama dengan 1000 = [1000]
+
+//10. gabungkan variable isObject kedalam variable arrayOf dengan menggunakan method push atau spread operator
+/**
+    output:
+     const arrayOf = [{
+        nama_mobil: "lamborgini",
+        kecepatan: "8000 cc",
+        harga: 70000000000,
+        tipe: "aventador"
+    },{
+        nama_mobil: "ferrari",
+        kecepatan: "6000 cc",
+        harga: 20000000000,
+        tipe: "tidak diketahui"
+    }]
+ * */
+
+//11. Buat metode Function() dari sebuah  semua methode algorithma yg telah di kerjakan lalu return setiap kondisinya dan berikan nama function yg sesuai denga kebutuhan
+/**
+    output : 
+        function(){
+            // isilah metode tersebut kedalam function()///
+            // berikan return valuenya
+        } 
+* */
